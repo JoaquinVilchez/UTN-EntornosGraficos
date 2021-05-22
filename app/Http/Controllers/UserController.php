46b430be519539916196306adef04dc53032e233
mariiocarande <mdc.mariio@gmail.com>
2021-05-22 19:01:41 -0300

    elimine vista para confirmar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -90,6 +90,7 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
+
     public function destroy($id)
     {
         $user = User::find($id);
--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/usuarios', [UserController::class, 'index'])->name('user.index');
 Route::get('/usuarios/editar/{id}', [UserController::class, 'edit'])->name('user.edit');
 Route::put('/usuarios/editar/{id}', [UserController::class, 'update'])->name('user.update');
-Route::delete('/usuarios/eliminar/{id}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::get('/usuarios/eliminar/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 
