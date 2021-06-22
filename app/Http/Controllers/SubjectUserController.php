e33664381f7f45ef95d1fa8c0c06fa525174260f
leandro zoch <leandro_zoch@hotmail.com>
2021-06-22 11:29:49 -0300

    Arreglo funcion recuperar docentes

--- app/Http/Controllers/SubjectUserController.php
+++ app/Http/Controllers/SubjectUserController.php
@@ -272,16 +272,16 @@ class SubjectUserController extends Controller
     public function view_teacher_subjects($id_user)
     {
         $user = User::find($id_user);
-        $userType = $user->getType();
-        if ($userType == 'Docente')
+        
+        if ($user->type == 'teacher')
         {
             $subjects = $user->subjects()->paginate(10);
             return view('subjects_user.view_teacher_subjects')->with('subjects', $subjects)->with('user',$user);   
         }
-        else{
+    else{
             
-            return redirect()->route('subjects_user.view_teacher_subjects', $id_user)->with('success_message', 'No es profesor.'); //ver linea, esta mal.
-         }
+            return back()->with('error_message', 'El usuario indicado no es docente.'); //ver linea, esta mal.
+        }
         
         
          
