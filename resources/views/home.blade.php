02811515002144b5da563b5281fc1a7dd12f89a1
Gerardo <gdcaciorgna@hotmail.com>
2021-06-29 22:49:20 -0300

    Se corrigieron errores y se actualizaron detalles de inscripciones

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,14 +12,14 @@
                             <img src="https://i.pinimg.com/originals/51/d0/a4/51d0a49eef5fb23029560f389f64a21e.jpg" alt="profile-picture" class="rounded-circle img-thumbnail" style="height: 100px;">
                         </div>
                         <div class="ml-4">
-                            <h1 class="card-title">¡Hola 
-                            @if (Auth::check())    
-                                {{Auth::user()->first_name}}                   
-                            @endif!</h1>
+                            <h1 class="card-title">¡Hola
+                                @if(Auth::check())    
+                                {{Auth::user()->first_name}}       
+                                @endif!</h1>
                             <p class="card-text">Estudiante de Ingeniería en Sistemas de Información</p>
                         </div>
                         <div class="d-flex align-items-center m-4">
-                            <button href="#" class="btn btn-primary">Editar Perfil</button>
+                            <a href="{{route('user.edit', Auth::user()->id)}}" class="btn btn-primary">Editar Perfil</a>
                         </div>
                         </div>
                         </div>
@@ -29,7 +29,9 @@
                     <a href="{{route('user.index')}}" class="btn btn-primary m-2">Usuarios</a>
                     <a href="{{route('users.search_teacher')}}" class="btn btn-primary m-2">Docentes</a>
                     <a href="{{route('subject.index')}}" class="btn btn-primary m-2">Materias</a>                    
-                    <a href="{{route('inscriptions_user.list')}}" class="btn btn-primary m-2">Mis inscripciones consultas</a>      
+                    <a href="{{route('inscriptions_user.list')}}" class="btn btn-primary m-2">Mis inscripciones consultas</a>
+                    <a href="{{route('meetings.list')}}" class="btn btn-primary m-2">Consultas</a>      
+      
 
                     @if (Auth::check())
                         <a href="{{route('subjects_user.index', Auth::user()->id)}}" class="btn btn-primary m-2">Mis inscripciones a materias</a> 
